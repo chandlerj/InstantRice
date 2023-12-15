350bb4aa798c9c8939b4b7c91dbee9e08f73edc4
chqn <chandler@cock.li>
2023-12-14 20:33:22 -0700

    img path is relative now

--- src/get_args.py
+++ src/get_args.py
@@ -1,14 +1,14 @@
 import user_interface
-
+import os
 def get_args(args):
-
+    
     dmenu = False
     nolock = False
 
     if '-r' in args:
         img_path = user_interface.pickRandomWallpaper()
     else:
-        img_path = args[1]
+        img_path = f"{os.getcwd()}/{args[1]}"
     
     if '-dmenu' in args:
         dmenu = True
