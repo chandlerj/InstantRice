52e15e642258a56166c62efe33fd40a773f97fec
Chandler J <cjustice2000@gmail.com>
2024-01-27 13:59:22 -0700

    rofi initializer progress

--- src/initialize_rofi.py
+++ src/initialize_rofi.py
@@ -7,14 +7,23 @@ def initializeRofi() -> None:
     """    
     print('[bold red]Initializing Rofi Theme')
     
-    uname = os.getlogin()
-    
-
+    uname: str = os.getlogin()
     # copy the default config from data to .config
-
+    configPath = f'/home/{uname}/.config/rofi/'
+    dirExists = os.path.isdir(configPath)
+    if dirExists:
+        print('path exists')
+        if os.path.exists(f'{configPath}config.rasi'):
+            print('config present')
+        else:
+            # create the rasi config
+            pass
+    else:
+        print('path doesnt exist')
     # add line to rofi config 
     # (this means we need might need to make the rofi config file, and the rofi directory)
     # (it also will be necessary to grab the current user's username to access their .config)
     print(uname)
+    print(dirExists)
 
 initializeRofi()
--- src/update_rofi.py
+++ src/update_rofi.py
@@ -9,16 +9,16 @@ def updateRofiTheme(config_path: str, colors: list, compliments: list) -> None:
     bg = color_engine.hexToRGB(colors[1])
     fg = color_engine.hexToRGB(compliments[1])
     lbg = color_engine.hexToRGB(colors[0])
-    lfg = color_engine.hexToRGB(colors[0])
+    lfg = color_engine.hexToRGB(colors[2])
     for i,line in enumerate(data):
-        if 'background: ' in line and i == 23:
-            data[i] = '    background: rgba({}, {}, {}, 70%);\n'.format(bg[0], bg[1], bg[2])
-        if 'foreground: ' in line and i == 28:
-            data[i] = '    foreground: rgba({}, {}, {}, 100%);\n'.format(fg[0], fg[1], fg[2])
-        if 'lightbg: ' in line and i == 11:
-            data[i] = '    lightbg: rgba({}, {}, {}, 100%);\n'.format(lbg[0], lbg[1], lbg[2])
-        if 'lightfg: ' in line and i == 7:
-            data[i] = '    lightfg: rgba({}, {}, {}, 100%);\n'.format(lfg[0], lfg[1], lfg[2])
+        if 'rice-background: ' in line:
+            data[i] = '    rice-background: rgba({}, {}, {}, 70%);\n'.format(bg[0], bg[1], bg[2])
+        if 'rice-foreground: ' in line:
+            data[i] = '    rice-foreground: rgba({}, {}, {}, 100%);\n'.format(fg[0], fg[1], fg[2])
+        if 'rice-lightbg: ' in line:
+            data[i] = '    rice-lightbg: rgba({}, {}, {}, 100%);\n'.format(lbg[0], lbg[1], lbg[2])
+        if 'rice-lightfg: ' in line:
+            data[i] = '    rice-lightfg: rgba({}, {}, {}, 100%);\n'.format(lfg[0], lfg[1], lfg[2])
     with open(config_path, 'w') as file:
         file.writelines(data)
 
