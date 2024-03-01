13ea9b7539534b19ce61d2694c0cf3832a6ec35e
Chandler J <cjustice2000@gmail.com>
2024-03-01 11:08:46 -0700

    created some ideas for things to do with project during hackusu

---
+++ src/appearance.py
@@ -0,0 +1,14 @@
+
+
+class Appearance:
+    """
+    Generic object to get configuration settings for a given program
+
+    path - path to the configuration file
+    """
+    def __init__(self, path):
+        print("hello from appearance")
+        self.path = path 
+    
+    def get(self):
+        pass
---
+++ src/get_i3_appearence.py
@@ -0,0 +1,12 @@
+
+from appearance import Appearance
+
+class i3_Appearence(Appearance):
+    
+    def __init__(self, path):
+        super().__init__(self)
+
+    def get(self):
+        pass
+
+test = i3_Appearence("test")
